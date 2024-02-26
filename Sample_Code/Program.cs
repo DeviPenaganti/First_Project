38c60959109f218a8c0753d0db9b711968e65139
PE20257086 <PE20257086@APL-5CD138LTQC>
2024-02-26 23:11:08 +0530

    Add project files.

---
+++ Sample_Code/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.VisualBasic;
+
+Console.WriteLine("Hello, World!");
+
+//initializing string with default value
+string name = String.Empty; //dynamic value
+int age = 0;
+
+Console.WriteLine("Please enter your Name and Age: ");
+name = Console.ReadLine();
+age= Convert.ToInt32(Console.ReadLine());
+
+//static message with dynamic value
+Console.WriteLine("I am " + name + "My age: "+age ); //string concatenation
+Console.WriteLine($"They called me {name}"); //string interpolation
+Console.WriteLine("I was given the name {0}", name); //formatted string
+
